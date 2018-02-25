d5a99cc1d87d08de64ec41b6e4f80e478d41a2db
tnonoyama <30341881+tnonoyama@users.noreply.github.com>
2018-02-25 16:35:24 +0900

    Add files via upload

---
+++ examples/example_camera_cal.py
@@ -0,0 +1,61 @@
+
+# coding: utf-8
+
+# ## Advanced Lane Finding Project
+# 
+# The goals / steps of this project are the following:
+# 
+# * Compute the camera calibration matrix and distortion coefficients given a set of chessboard images.
+# * Apply a distortion correction to raw images.
+# * Use color transforms, gradients, etc., to create a thresholded binary image.
+# * Apply a perspective transform to rectify binary image ("birds-eye view").
+# * Detect lane pixels and fit to find the lane boundary.
+# * Determine the curvature of the lane and vehicle position with respect to center.
+# * Warp the detected lane boundaries back onto the original image.
+# * Output visual display of the lane boundaries and numerical estimation of lane curvature and vehicle position.
+# 
+# ---
+# ## First, I'll compute the camera calibration using chessboard images
+
+# In[3]:
+
+
+import numpy as np
+import cv2
+import glob
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'qt')
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d points in real world space
+imgpoints = [] # 2d points in image plane.
+
+# Make a list of calibration images
+images = glob.glob('../camera_cal/calibration*.jpg')
+
+# Step through the list and search for chessboard corners
+for fname in images:
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    # Find the chessboard corners
+    ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
+
+    # If found, add object points, image points
+    if ret == True:
+        objpoints.append(objp)
+        imgpoints.append(corners)
+
+        # Draw and display the corners
+        img = cv2.drawChessboardCorners(img, (9,6), corners, ret)
+        cv2.imshow('img',img)
+        cv2.waitKey(500)
+
+cv2.destroyAllWindows()
+
+
+# ## And so on and so forth...
